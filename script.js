2f3d98562708fb6a5b6b309c8e9518c07429b49d
unitlytools <unitlytools@gmail.com>
2025-11-21 10:57:33 +0530

    Add interactive background color change feature

---
+++ script.js
@@ -0,0 +1,12 @@
+import Grid1Background from 'https://cdn.jsdelivr.net/npm/threejs-components@0.0.16/build/backgrounds/grid1.cdn.min.js'
+
+const bg = Grid1Background(document.getElementById('webgl-canvas'))
+
+const button1 = document.getElementById('colors-btn')
+button1.addEventListener('click', () => {
+  bg.grid.setColors([0xffffff * Math.random(), 0xffffff * Math.random(), 0xffffff * Math.random()])
+  bg.grid.light1.color.set(0xffffff * Math.random())
+  bg.grid.light1.intensity = 500 + Math.random() * 1000
+  bg.grid.light2.color.set(0xffffff * Math.random())
+  bg.grid.light2.intensity = 250 + Math.random() * 250
+})
